a56c8e6e29aa489c6efa53e6e8bd613b988f5944
tigabriel09-coder <ti.gabriel09@gmail.com>
2025-11-12 11:32:00 -0300

    start 2

--- start.py
+++ start.py
@@ -27,3 +27,31 @@ def inicial(tela):
     caminho_imagem = os.path.join('imagens', "tela_inicial2.png")
     fundo = pygame.image.load(caminho_imagem)
     fundo2 = pygame.transform.scale(fundo, (600,700))
+
+    #funcao carrega sons
+    cache_sons = {}
+    def carrega_sons(som):
+        if som not in cache_sons:
+            try:
+                caminho = os.path.join(os.path.dirname(__file__), som)
+                cache_sons[som] = pygame.mixer.Sound(caminho)
+            except:
+                print('erro ao tentar ler arquivo de som: {0}' .format(som))
+                sys.exit()
+        return cache_sons[som]
+
+    arquivo = os.path.join('sons', 'My_Chemical_Romance_Helena_8-Bit.mp3')
+    caminho = os.path.join(os.path.dirname(__file__), arquivo)
+    pygame.mixer.music.load(caminho)
+    pygame.mixer.music.play(-1)
+    pygame.mixer.music.set_volume(0.8)    
+
+    som_acao = carrega_sons(arquivo)
+
+    game = True
+
+    #loop principal
+    while game:
+
+        clock.tick(60)
+        eventos = pygame.event.get()
